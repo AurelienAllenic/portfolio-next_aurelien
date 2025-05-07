4799c868086d2545b19558409e1d775447accf33
AurelienAllenic <aurelien.allenic@gmail.com>
2025-05-07 17:23:26 +0200

    (feat) added confidentiality page

---
+++ src/app/confidentiality/page.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import React from 'react'
+import styles from './confidentiality.module.scss'
+
+const Confidentiality = () => {
+  return (
+    <section className={styles.confidentiality}>
+      <h1 className={styles.title_confidentiality}>
+        Règles de confidentialité pour l'application MagicCounters
+      </h1>
+      <ul className={styles.content_confiddentiality}>
+        <li>I. Aucune information personnelle n'est collectée ou partagée par l'application</li>
+        <li>II. Expo est utilisé en tant que bibliothèque SDK pour build l'application et rien de plus</li>
+        <li>III. Aucune donnée utilisateur n'est transmise depuis mon serveur via un tiers ou une autre application tierce sur le même appareil</li>
+        <li>IV. La seule interaction de l'utilisateur hors de l'application est une requête à l'api de Scryfall afin d'afficher l'image de son choix en fond</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Confidentiality
